3bab2daabf9bf1412a8f2c045d52f9b5e1132fe1
prrcomputers <98372244+prrcomputers@users.noreply.github.com>
2025-08-11 16:50:35 -0400

    Compute colors from HSV instead of sampling wheel

--- CTkColorPicker/ctk_color_picker_widget.py
+++ CTkColorPicker/ctk_color_picker_widget.py
@@ -224,7 +224,6 @@ class CTkColorPicker(customtkinter.CTkFrame):
             getattr(self, "target_x", 0),
             getattr(self, "target_y", 0),
             brightness,
-            self.rgb_color[:],
             self.slider,
             self.entry,
             command=self.command,
